b5bc2453850231e84f78b5ff584888465fe9c7dc
lizawood <liza.4.bc@gmail.com>
2018-11-22 08:04:57 -0800

    Updated data module - with inheritance

--- myfitness/read/data.py
+++ myfitness/read/data.py
@@ -1,12 +1,19 @@
-class readdata:
-    def __init__(self, file, name):
-        self.file = file
+class Person:
+    def __init__(self, name, age, gender):
         self.name = name
+        self.age = age
+        self.gender = gender
+
+    def display(self):
+        print("Name: {}, Age: {}, Gender: {}". format(self.name, self.age, self.gender))
+
+class healthdata(Person):
+    def __init__(self, name, age, gender, file):
+        Person.__init__(self, name, age, gender)
+        self.file = file
 
     def data(self):
         import pandas as pd
         self.data = pd.read_csv(self.file)
+        Person.display(self)
         return self.data
-
-    def getname(self):
-        return self.name
