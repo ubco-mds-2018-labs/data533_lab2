cc997af9b0cfa831dd36bc9787d89d0170b883fc
lizawood <liza.4.bc@gmail.com>
2018-11-23 13:16:07 -0800

    Genericized chart module

--- myfitness/healthdata/chart.py
+++ myfitness/healthdata/chart.py
@@ -1,16 +1,16 @@
-def chart(data):
+def chart(columnX, columnY, xlabel, ylabel):
     import pygal
     from IPython.display import SVG, display
 
-    y = data['steps']
-    x = data['date']
+    y = columnX
+    x = columnY
     bar_chart = pygal.Bar(include_x_axis=True)
-    bar_chart.add('Steps',y)
-    bar_chart.title = 'Average steps per month'
+    bar_chart.add(ylabel,y)
+    bar_chart.title = 'My Fitness Chart'
     bar_chart.x_labels = x
-    bar_chart.x_title = "Month"
-    bar_chart.y_title = "Average Steps"
+    bar_chart.x_title = xlabel
+    bar_chart.y_title = ylabel
 
-    bar_chart.render_to_file('my_health_data.svg')
+    bar_chart.render_to_file('my_fitness_data.svg')
 
     bar_chart.render_in_browser()
