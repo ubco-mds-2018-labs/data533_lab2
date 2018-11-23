caffed6d982b2dad9386f42bcab098470c86a71c
lizawood <liza.4.bc@gmail.com>
2018-11-23 12:11:27 -0800

    Adding tests for modules in summary package and in-code documentation for each module

--- myfitness/summary/maxMin.py
+++ myfitness/summary/maxMin.py
@@ -5,12 +5,32 @@
 
 
 def getMax(data):
-    import pandas as pd
+    """ Find the maximum number of steps in the data and the date it was achieved.
+        Parameters:
+            data: Pandas DataFrame containing Apple Health data imported from a
+                  .csv file.
+        Return:
+            The row of values for when the maximum number of steps were achieved:
+            Start date, Finish date, Distance(mi), Steps (count)"""
+
+
+    import pandas as pd             # ensure pandas has been imported
+
+    # find the row containing the maximum steps and return that row.
     maximum = data.loc[data['Steps (count)'].idxmax()]
     return maximum
 
 def getMin(data):
-    import pandas as pd
+    """ Find the maximum number of steps in the data and the date it was achieved.
+        Parameters:
+            data: Pandas DataFrame containing Apple Health data imported from a
+                  .csv file.
+        Return:
+            The row of values for when the maximum number of steps were achieved:
+            Start date, Finish date, Distance(mi), Steps (count)"""
+
+    import pandas as pd             #ensure pandas has been imported
+
+    # find the row containing the minimum steps and return that row.
     minimum = data.loc[data['Steps (count)'].idxmin()]
     return minimum
-
