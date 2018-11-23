28c3e022f3c13d3de852edd06e7dfdce16b564c1
LevanniaLildhar <LevanniaLildhar>
2018-11-23 14:59:27 -0800

    Commented chart module

--- myfitness/healthdata/chart.py
+++ myfitness/healthdata/chart.py
@@ -1,9 +1,18 @@
 def chart(columnX, columnY, xlabel, ylabel):
-    import pygal
+    """Create an interactive bar graph using pygal
+        Parameters: columnX as list of strings, columnY as list of values, xlabel as string, ylabel as string
+        
+        Return: .svg file with xlabel, ylabel and title"""
+    
+    # import pygal, import SVG, and display from IPython.display
+    import pygal 
     from IPython.display import SVG, display
 
+    # assign lists provided to chart axises
     y = columnY
     x = columnX
+    
+    # construct pygal bar chart
     bar_chart = pygal.Bar(include_x_axis=True)
     bar_chart.add(ylabel,y)
     bar_chart.title = 'My Fitness Chart'
@@ -11,6 +20,7 @@ def chart(columnX, columnY, xlabel, ylabel):
     bar_chart.x_title = xlabel
     bar_chart.y_title = ylabel
 
+    # render bar chart to file and brower for viewing
     bar_chart.render_to_file('my_fitness_data.svg')
 
     bar_chart.render_in_browser()
